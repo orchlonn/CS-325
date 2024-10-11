52ab117ff999a10788420d641074aade1282061d
orchlonn <ch.orchlon@gmail.com>
2024-10-10 21:11:29 -0700

    lab - 2 first stage is finished

---
+++ lab - 2/memory.asm
@@ -0,0 +1,27 @@
+; question 1
+        segment .data
+a       db      76
+b       dw      76
+c       dd      76
+d       dd      20.123
+e       dd      0x172F2
+
+; question 2
+        segment .bss
+g       resw    20 
+h       resb    30
+i       resd    5
+
+        section .text
+        global  main        ; let the linker know about main
+main:
+        push    rbp         ; set up a strack frame for main
+        mov     rbp, rsp    ; let rbp point to the previous ssp
+        sub     rsp, 16     ; leave some room for local variables
+                            ; and align stack to 16 byte boundary
+        xor     eax, eax    ; set rax to 0 for return value
+        leave               ; undo the stack manipulations
+        ret
+        
+        
+
